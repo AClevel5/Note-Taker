fcd1d1a839b007dc5ddaef504ec036da5264f9ae
Alex Cleveland <Alexcleveland5@gmail.com>
2022-09-05 20:13:52 -0600

    Notes Route

--- routes/notes.js
+++ routes/notes.js
@@ -1,8 +1,38 @@
+//Require express and helper files
 const express = require('express');
 const uuid = require('../helpers/uuid');
-const { readFromFile, writeToFile, readAndAppend } = require('../helpers/fsUtils');
+const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
 const notes = express.Router();
 
+//Get to retrieve exisiting notes
 notes.get('/', (req, res) =>
     readFromFile('../db/db.json').then((data) => res.json(JSON.parse(data)))
 );
+
+//Post to add new notes
+notes.post('/', (req, res) => {
+    //Destructuring
+    const { title, text } = req.body;
+
+    if (title && text) {
+        const newNote = {
+            title,
+            text,
+            note_id: uuid(),
+        };
+
+        readAndAppend(newNote, './db/db.json');
+
+        const response = {
+            status: "Sucess",
+            body: newNote,
+        };
+
+        res.json(response);
+    } else {
+        res.json("Error in posting Note");
+    }
+
+});
+
+module.exports = notes;
